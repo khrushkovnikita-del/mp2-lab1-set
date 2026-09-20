cf5cd485683aea4c25b8780d4f2979d8701cb5a8
StepanKhrushkov <zefirkasneba@mail.ru>
2026-09-21 01:50:13 +0300

    выполнил tbitfield.cpp

---
+++ build/gtest/googletest-1.18.0/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: D:/repo/mp2-lab1-set/gtest/googletest-1.18.0
+# Build directory: D:/repo/mp2-lab1-set/build/gtest/googletest-1.18.0
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("googletest")
---
+++ build/gtest/googletest-1.18.0/googletest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: D:/repo/mp2-lab1-set/gtest/googletest-1.18.0/googletest
+# Build directory: D:/repo/mp2-lab1-set/build/gtest/googletest-1.18.0/googletest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
